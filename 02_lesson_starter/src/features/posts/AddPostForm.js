30b736de89956d31ffc8e5b2361d06c7720a5a72
3nonch <enonch@hotmail.com>
2024-03-12 20:22:56 +0300

    lesson 2 completed!

--- 02_lesson_starter/src/features/posts/AddPostForm.js
+++ 02_lesson_starter/src/features/posts/AddPostForm.js
@@ -1,30 +1,35 @@
-import { nanoid } from "@reduxjs/toolkit";
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const [userId, setUserId] = useState("");
+  const users = useSelector(selectAllUsers);
+
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
   const onSavePostClicked = (e) => {
     e.preventDefault();
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content,
-        })
-      );
-
+      dispatch(postAdded(title, content, userId));
       setTitle("");
       setContent("");
     }
   };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const usersOptions = users.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ));
 
   const dispatch = useDispatch();
   return (
@@ -39,6 +44,15 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <select
+          name="postAuthor"
+          id="postAuthor"
+          value={userId}
+          onChange={onAuthorChanged}
+        >
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           name="postContent"
@@ -46,7 +60,9 @@ const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         ></textarea>
-        <button type="submit">Save Post</button>
+        <button disabled={!canSave} type="submit">
+          Save Post
+        </button>
       </form>
     </section>
   );
